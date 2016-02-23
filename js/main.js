c312d248ebc87ccb0c30357fbf4b3fdaf1dcaa4b
Юрий <yuriiall@mail.ru>
2016-02-23 20:24:55 +0300

    first

---
+++ js/main.js
@@ -0,0 +1,14 @@
+require.config({
+    paths:{
+        "jquery":"libs/jquery",
+        "fabric":"libs/fabric.all.min",
+        "backbone":"libs/backbone",
+        "underscore":"libs/underscore",
+        "menu":"game.menu",
+        "game":"game.start"
+    }
+});
+
+require(["jquery","underscore","backbone","fabric","menu","game"],function($,undescore,backbone,fabric,menu,game){
+    //игра загружена
+});
